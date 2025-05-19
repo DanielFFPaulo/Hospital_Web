c71f3570df0bac90c13172017b4b6cb29e8e9f3d
SofiaVedor05 <sofia.vedor.05@gmail.com>
2025-05-19 22:14:38 +0100

    Adicionar categorias no registo

--- Hospital_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Hospital_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -98,6 +98,10 @@ namespace Hospital_Web.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Categoria")]
+            public string Categoria { get; set; }
         }
 
 
@@ -119,9 +123,14 @@ namespace Hospital_Web.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
+
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    // Atribuir a Role (categoria)
+                    await _userManager.AddToRoleAsync(user, Input.Categoria);
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
