f2984172f85bdd832c7c40660ef9108fee2c403c
SofiaVedor05 <sofia.vedor.05@gmail.com>
2025-06-12 21:25:15 +0100

    corregar do controler pessoas

--- Hospital_Web/Controllers/PessoasController.cs
+++ Hospital_Web/Controllers/PessoasController.cs
@@ -24,22 +24,9 @@ namespace Hospital_Web.Controllers
         }
 
         // GET: Pessoas
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index()
         {
-            if (_context.Pessoa == null)
-            {
-                return Problem("Entity set 'Hospital_WebContext.Pessoa'  is null.");
-            }
-
-            var pessoas = from p in _context.Pessoa
-                         select p;
-
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                pessoas = pessoas.Where(s => s.Nome!.ToUpper().Contains(searchString.ToUpper()));
-            }
-
-            return View(await pessoas.ToListAsync());
+            return View(await _context.Pessoa.ToListAsync());
         }
 
         // GET: Pessoas/Details/5
@@ -65,7 +52,7 @@ namespace Hospital_Web.Controllers
         // POST: Pessoas/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("N_Processo,Nome,DataDeNascimento,sexo,Morada,Grupo_Sanguineo,Telemovel,TelemovelAlt,Email,NIF,Cod_Postal,Localidade")] Pessoa pessoa)
+        public async Task<IActionResult> Create([Bind("N_Processo,Nome,Idade,Data_de_Nascimento,Morada,Telemovel,TelemovelAlt,Email,NIF,Cod_Postal,Localidade")] Pessoa pessoa)
         {
             if (!ModelState.IsValid)
                 return View(pessoa);
@@ -88,35 +75,7 @@ namespace Hospital_Web.Controllers
                 return NotFound();
 
             return View(pessoa);
-       }
-        // POST: Pessoas/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("N_Processo,Nome,DataDeNascimento,sexo,Morada,Grupo_Sanguineo,Telemovel,TelemovelAlt,Email,NIF,Cod_Postal,Localidade")] Pessoa pessoa)
-        {
-            if (id != pessoa.N_Processo)
-                return NotFound();
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(pessoa);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PessoaExists(pessoa.N_Processo))
-                        return NotFound();
-                    else
-                        throw;
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(pessoa);
-      
+        }
 
 
         // GET: Pessoas/Delete/5
