a3c1c79428b371771415751fb842d11b0a601c6c
SofiaVedor05 <sofia.vedor.05@gmail.com>
2025-06-19 16:09:26 +0100

    Página Info

--- Hospital_Web/Controllers/HomeController.cs
+++ Hospital_Web/Controllers/HomeController.cs
@@ -20,6 +20,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Info()
+{
+    return View();
+}
 
     public IActionResult Privacy()
     {
